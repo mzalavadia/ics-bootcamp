64ea8b59cfbf3fb1fc8bf00eb75217084c24bb88
mzalavadia <34287192+mzalavadia@users.noreply.github.com>
2017-12-22 14:56:53 -0800

    Delete fullNameGreeting.rb

--- fullNameGreeting.rb
+++
@@ -1,7 +0,0 @@
-puts 'Hello there what\'s your first name?'
-name1 = gets.chomp
-puts 'Nice to meet you ' + name1 + '. And what is your middle name? '
-name2 = gets.chomp
-puts 'Wow what a nice middle name! To wrap it all up, what is your last name?'
-name3 = gets.chomp
-puts 'Nice to meet you ' + name1 +' '+ name2 +' '+ name3
